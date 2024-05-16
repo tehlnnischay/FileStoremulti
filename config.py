102713c0ffcec5f61ac8c481d277610136039e66
tehlnnischay <tehlannischay0001@gmail.com>
2024-05-16 20:29:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,6 +38,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "HDMEDIA03")
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001935247305"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002144760143"))
 FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002113909424"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002067058871"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
