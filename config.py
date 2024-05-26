f223d9858b7970790dc7229e4391ccff3598c980
tehlnnischay <tehlannischay0001@gmail.com>
2024-05-26 08:21:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,6 +36,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "HDMEDIA03")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001935247305"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
