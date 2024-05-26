162f956de094e0483002d9ec38224f25d3a67e6f
tehlnnischay <tehlannischay0001@gmail.com>
2024-05-26 08:06:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "HDMEDIA03")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001935247305"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002144760143"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002113909424"))
 FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002067058871"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
